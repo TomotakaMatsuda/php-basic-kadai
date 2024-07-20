c32d5a64f8fbd51d5f3eb9fee5c5f7c4eb9b9be7
TomotakaMatsuda <61697097+TomotakaMatsuda@users.noreply.github.com>
2024-07-20 14:04:55 +0900

    Add kadai_007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編kadai_007</title>
+</head>
+
+<body>
+    <p>
+        <?php
+            //配列を作成
+            $array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+            //配列を出力
+            print_r($array);
+        ?>
+    </p>
+</body>
+
+</html>
